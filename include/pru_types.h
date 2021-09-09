dac8a110ee6a11c76239c60051d66c6b8fa12d0d
Nick Saulnier <nsaulnier@ti.com>
2021-09-09 14:44:28 -0500

    include: Port pru_types to Linux 5.10
    Linux 5.10 RemoteProc driver expects INTC information to be provided in an INTC
    map structure. See Linux drivers/remoteproc/pru_rproc.h for details.
    
    This commit adds structures pruss_int_map and pru_irq_rsc to
    include/pru_types.h.
    
    Linux 5.10 pruss_int_map provides PRU INTC mapping information for
    system event --> channel --> host interrupt.
    
    Linux 5.4 ch_map only provided system event --> channel mapping. ch_map will be
    deprecated in a later commit.
    
    Signed-off-by: Nick Saulnier <nsaulnier@ti.com>

--- include/pru_types.h
+++ include/pru_types.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2018 Texas Instruments Incorporated - http://www.ti.com/
+ * Copyright (C) 2015-2021 Texas Instruments Incorporated - http://www.ti.com/
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -38,6 +38,47 @@
 
 #define PRU_INTS_VER0		(0 << 16)
 #define PRU_INTS_VER1		(1 << 16)
+
+/**
+ * struct pruss_int_map - sysevt to channel to host interrupt mapping
+ *
+ * @event: the number of the sysevt
+ * @chnl: channel number assigned to a given @event
+ * @host: the host interrupt assigned to a given @chnl
+ *
+ * PRU system events are mapped to channels, and these channels are mapped to
+ * hosts. Events can be mapped to channels in a one-to-one or many-to-one ratio
+ * (multiple events per channel), and channels can be mapped to hosts in a
+ * one-to-one or many-to-one ratio (multiple events per channel).
+ *
+ * @event is the number of the sysevt, @chnl is the number of the channel to be
+ * mapped, and @host is the number of the host interrupt to be mapped.
+ *
+ * pruss_int_map is defined in Linux at drivers/remoteproc/pru_rproc.h
+ */
+struct pruss_int_map {
+	uint8_t event;
+	uint8_t chnl;
+	uint8_t host;
+};
+
+/**
+ * struct pru_irq_rsc - PRU firmware section header for IRQ data
+ * @type: resource type
+ * @num_evts: number of described events
+ * @pru_intc_map: PRU interrupt routing description
+ *
+ * The PRU firmware blob can contain optional .pru_irq_map ELF section, which
+ * provides the PRUSS interrupt mapping description. The pru_irq_rsc struct
+ * describes resource entry format.
+ *
+ * pru_irq_rsc is defined in Linux at drivers/remoteproc/pru_rproc.h
+ */
+struct pru_irq_rsc {
+	uint8_t type;
+	uint8_t num_evts;
+	struct pruss_int_map pru_intc_map[];
+};
 
 /**
  * struct ch_map - sysevts-to-channel mapping
