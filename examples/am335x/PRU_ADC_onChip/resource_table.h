8c961a4def32cb0194325d033c5621a717882b85
Nick Saulnier <nsaulnier@ti.com>
2021-10-13 22:21:40 -0500

    am335x: Port to Linux 5.10
    Port AM335x projects to be compatible with Linux 5.10 RemoteProc driver.
    AM335x projects in these folders were updated:
    * examples/am335x
     * labs/Hands_on_Labs
     * labs/Getting_Started_Labs
    
    Note that the Linux PRU RPMsg driver was NOT ported to Linux 5.10 for
    AM64x/AM65x TI Processor Linux SDK 8.0. Linux PRU RPMsg will be supported in
    future TI Processor Linux SDK 8.x releases.
    
    The Linux RemoteProc driver loads PRU cores slightly differently in Linux kernel
    5.10 than it did in Linux 5.4:
     * Resource tables are no longer required if RPMsg is not used (i.e., "empty"
       resource tables are no longer needed)
     * INTC configuration is passed to the RemoteProc driver in a different
       structure than the resource table
     * PRU firmware provides INTC configuration for interrupts going to the ICSS or
       ICSSG, while Linux client driver provides INTC configuration for interrupts
       going to the Linux host. Note that PRU firmware can also still manually
       configure the INTC within the PRU code. However, configuration for interrupts
       going to the Linux host should still be configured by Linux only. This allows
       the PRU Linux drivers to verify that there are no conflicting interrupt
       settings across different PRU Linux applications.
    
    Changes in this commit:
     * split resource_table_X.h into intc_map_X.h and resource_table.h
     * RPMsg projects: intc_map_X.h only sets the kick interrupt. The vring
       interrupt is now set in the Linux device tree
     * Change linker command file naming conventions:
       - AM335x_PRU.cmd is the basic linker command file
       - Any linker command file that is different will have a different name. e.g.,
         * AM335x_PRU_intc_rscTbl.cmd adds intc_map and resource table
         * AM335x_PRU_intc.cmd adds just an intc_map
     * Update Makefile:
       - use new linker command file names
       - Remove RPMsg library from projects without RPMsg
       - update comments and debug messages
     * Update ReadMe file:
       - document examples
     * Update examples/PRU_Direct_ConnectX
       - remove unused INTC mapping
       - add additional interrupt signalling
       - add additional comments to PRU firmware
    
    Signed-off-by: Nick Saulnier <nsaulnier@ti.com>

--- examples/am335x/PRU_ADC_onChip/resource_table.h
+++ examples/am335x/PRU_ADC_onChip/resource_table.h
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2015-2018 Texas Instruments Incorporated - http://www.ti.com/
- *
+ * Copyright (C) 2021 Texas Instruments Incorporated - http://www.ti.com/
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -31,17 +30,8 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/*
- *  ======== rsc_table_pru.h ========
- *
- *  Define the resource table entries for all PRU cores. This will be
- *  incorporated into corresponding base images, and used by the remoteproc
- *  on the host-side to allocated/reserve resources.
- *
- */
-
-#ifndef _RSC_TABLE_PRU_H_
-#define _RSC_TABLE_PRU_H_
+#ifndef _RESOURCE_TABLE_H_
+#define _RESOURCE_TABLE_H_
 
 #include <stddef.h>
 #include <rsc_types.h>
@@ -51,53 +41,70 @@
  * Sizes of the virtqueues (expressed in number of buffers supported,
  * and must be power of 2)
  */
-#define PRU_RPMSG_VQ0_SIZE	2
-#define PRU_RPMSG_VQ1_SIZE	2
+#define PRU_RPMSG_VQ0_SIZE	16
+#define PRU_RPMSG_VQ1_SIZE	16
 
-/* flip up bits whose indices represent features we support */
-#define RPMSG_PRU_C0_FEATURES	1
+/*
+ * The feature bitmap for virtio rpmsg
+ */
+#define VIRTIO_RPMSG_F_NS	0		//name service notifications
+
+/* This firmware supports name service notifications as one of its features */
+#define RPMSG_PRU_C0_FEATURES	(1 << VIRTIO_RPMSG_F_NS)
 
 /* Definition for unused interrupts */
 #define HOST_UNUSED		255
-
-/* Mapping sysevts to a channel. Each pair contains a sysevt, channel */
-struct ch_map pru_intc_map[] = { {16, 1},
-};
 
 struct my_resource_table {
 	struct resource_table base;
 
 	uint32_t offset[1]; /* Should match 'num' in actual definition */
 
-	/* intc definition */
-	struct fw_rsc_custom pru_ints;
+	/* rpmsg vdev entry */
+	struct fw_rsc_vdev rpmsg_vdev;
+	struct fw_rsc_vdev_vring rpmsg_vring0;
+	struct fw_rsc_vdev_vring rpmsg_vring1;
 };
 
-#pragma DATA_SECTION(am335x_pru_remoteproc_ResourceTable, ".resource_table")
-#pragma RETAIN(am335x_pru_remoteproc_ResourceTable)
-struct my_resource_table am335x_pru_remoteproc_ResourceTable = {
-	1,	/* we're the first version that implements this */
+#pragma DATA_SECTION(resourceTable, ".resource_table")
+#pragma RETAIN(resourceTable)
+struct my_resource_table resourceTable = {
+	1,	/* Resource table version: only version 1 is supported by the current driver */
 	1,	/* number of entries in the table */
 	0, 0,	/* reserved, must be zero */
 	/* offsets to entries */
 	{
-		offsetof(struct my_resource_table, pru_ints),
+		offsetof(struct my_resource_table, rpmsg_vdev),
 	},
 
+	/* rpmsg vdev entry */
 	{
-		TYPE_POSTLOAD_VENDOR, PRU_INTS_VER0 | TYPE_PRU_INTS,
-		sizeof(struct fw_rsc_custom_ints),
-		{
-			0x0000,
-			/* Channel-to-host mapping, 255 for unused */
-			HOST_UNUSED, 1, HOST_UNUSED, HOST_UNUSED, HOST_UNUSED,
-			HOST_UNUSED, HOST_UNUSED, HOST_UNUSED, HOST_UNUSED, HOST_UNUSED,
-			/* Number of evts being mapped to channels */
-			(sizeof(pru_intc_map) / sizeof(struct ch_map)),
-			/* Pointer to the structure containing mapped events */
-			pru_intc_map,
-		},
+		(uint32_t)TYPE_VDEV,                    //type
+		(uint32_t)VIRTIO_ID_RPMSG,              //id
+		(uint32_t)0,                            //notifyid
+		(uint32_t)RPMSG_PRU_C0_FEATURES,	//dfeatures
+		(uint32_t)0,                            //gfeatures
+		(uint32_t)0,                            //config_len
+		(uint8_t)0,                             //status
+		(uint8_t)2,                             //num_of_vrings, only two is supported
+		{ (uint8_t)0, (uint8_t)0 },             //reserved
+		/* no config data */
+	},
+	/* the two vrings */
+	{
+		FW_RSC_ADDR_ANY,        //da, will be populated by host, can't pass it in
+		16,                     //align (bytes),
+		PRU_RPMSG_VQ0_SIZE,     //num of descriptors
+		0,                      //notifyid, will be populated, can't pass right now
+		0                       //reserved
+	},
+	{
+		FW_RSC_ADDR_ANY,        //da, will be populated by host, can't pass it in
+		16,                     //align (bytes),
+		PRU_RPMSG_VQ1_SIZE,     //num of descriptors
+		0,                      //notifyid, will be populated, can't pass right now
+		0                       //reserved
 	},
 };
 
-#endif /* _RSC_TABLE_PRU_H_ */
+#endif /* _RESOURCE_TABLE_H_ */
